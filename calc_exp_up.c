ebc55638a8d287d4789a460cac9eb2045c7bb6cd
aquila57 <58740691+aquila57@users.noreply.github.com>
2019-12-23 20:17:23 -0600

    Corrected reference to header file

--- calc_exp_up.c
+++ calc_exp_up.c
@@ -18,7 +18,7 @@
    /* 59 Temple Place - Suite 330                                    */
    /* Boston, MA 02111-1307, USA.                                    */
 
-#include "runs.h"
+#include "runsa.h"
 
 double calc_exp_up(int indx, double N)
    {
